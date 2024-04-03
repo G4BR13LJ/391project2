cccd01b911bf4db948b283a670341c09426b97ed
Gabriel Jorand <gabrieljorand@gmail.com>
2024-04-03 12:31:39 -0600

    added correl scripts

--- mergeFilesAndScript/gabes scripts/shots_to_win_correlationGJ.py
+++ mergeFilesAndScript/gabes scripts/shots_to_win_correlationGJ.py
@@ -4,15 +4,15 @@ import pandas as pd
 df = pd.read_csv('../master_dataset.csv')
 
 # Calculate the total number of occurrences for each value in the 'HC' column
-hc_counts = df['HC'].value_counts()
+hc_counts = df['HS'].value_counts()
 
 # Create a DataFrame to store the probabilities
-prob_df = pd.DataFrame(columns=['HC', 'Probability of H in FTR'])
+prob_df = pd.DataFrame(columns=['HS', 'Probability of H in FTR'])
 
 # Iterate over unique values in the 'HC' column
-for hc_value in sorted(df['HC'].unique()):
+for hc_value in sorted(df['HS'].unique()):
     # Filter the DataFrame based on the 'HC' value
-    filtered_df = df[df['HC'] == hc_value]
+    filtered_df = df[df['HS'] == hc_value]
 
     # Count the occurrences of 'H' in the 'FTR' column
     h_count = filtered_df[filtered_df['FTR'] == 'H'].shape[0]
@@ -27,20 +27,20 @@ for hc_value in sorted(df['HC'].unique()):
     prob_df.loc[len(prob_df)] = [hc_value, probability]
 
 # Print the result
-print("Probability of 'H' in FTR based on HC value:")
+print("Probability of 'H' in FTR based on HS value:")
 print(prob_df)
 
 # ---------------------------------------------------------------------------------
 # Calculate the total number of occurrences for each value in the 'HC' column
-ac_counts = df['AC'].value_counts()
+ac_counts = df['AS'].value_counts()
 
 # Create a DataFrame to store the probabilities
-prob1_df = pd.DataFrame(columns=['AC', 'Probability of A in FTR'])
+prob1_df = pd.DataFrame(columns=['AS', 'Probability of A in FTR'])
 
 # Iterate over unique values in the 'AC' column
-for ac_value in sorted(df['AC'].unique()):
+for ac_value in sorted(df['AS'].unique()):
     # Filter the DataFrame based on the 'HC' value
-    filtered1_df = df[df['AC'] == ac_value]
+    filtered1_df = df[df['AS'] == ac_value]
 
     # Count the occurrences of 'H' in the 'FTR' column
     a_count = filtered1_df[filtered1_df['FTR'] == 'A'].shape[0]
@@ -55,5 +55,5 @@ for ac_value in sorted(df['AC'].unique()):
     prob1_df.loc[len(prob1_df)] = [ac_value, probability]
 
 # Print the result
-print("Probability of 'A' in FTR based on AC value:")
+print("Probability of 'A' in FTR based on AS value:")
 print(prob1_df)
